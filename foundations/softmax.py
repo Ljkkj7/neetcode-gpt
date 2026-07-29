d1e2e6f4987433f5514aae6085676265c01ddca5
Kian Jenkins <45104624+Ljkkj7@users.noreply.github.com>
2026-07-29 21:52:41 +0100

    Implement Softmax function (Math Foundations 3/6)

---
+++ foundations/softmax.py
@@ -0,0 +1,18 @@
+import numpy as np
+from numpy.typing import NDArray
+
+
+class Solution:
+
+    def softmax(self, z: NDArray[np.float64]) -> NDArray[np.float64]:
+        # z is a 1D NumPy array of logits
+        # Hint: subtract max(z) for numerical stability before computing exp
+        # return np.round(your_answer, 4)
+        exp_sum = sum(np.round(np.exp(z-max(z)), 4))
+        print(exp_sum)
+        output = []
+        for i in z:
+            output.append(float(np.round((np.exp(i-max(z))/exp_sum), 4)))
+        return output
+        
+
